95e0c4f3593e94621e1ede4398b2ca93be085a57
Zoltan Toth <zoltan.toth@prezi.com>
2015-09-14 09:50:03 +0200

    working error reporting, needs some more test though

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,7 @@ import org.python.antlr.ParseException;
 import org.python.core.CompilerFlags;
 import org.python.core.ParserFacade;
 import org.python.core.PyException;
+import org.python.core.PyObject;
 
 
 
@@ -13,7 +14,7 @@ public class App {
 		try {
 			ParserFacade.parseExpressionOrModule(new FileReader("correct.py"),"correct.py",new CompilerFlags().combine(CompilerFlags.PyCF_ONLY_AST));
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			System.err.println("File not found");
 			e.printStackTrace();
 		} catch (PyException e){
 			e.getMessage();
@@ -22,12 +23,16 @@ public class App {
 		try {
 			ParserFacade.parseExpressionOrModule(new FileReader("incorrect.py"),"incorrect.py",new CompilerFlags().combine(CompilerFlags.PyCF_ONLY_AST));
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			System.err.println("File not found");
 			e.printStackTrace();
-		} catch (ParseException e){
-			System.err.println(e.getMessage());
+		} catch (PyException e){
+			//e.normalize();
+			PyObject v = e.value;
+			String message = v.__getitem__(0).toString();
+			String line = v.__getitem__(1).__getitem__(1).toString();
+			System.err.println("At line " + line + ": " + message);
 		}
-		catch (Exception e){
+		catch (ParseException e){
 			System.err.println(e.getClass());
 		}
 	}
